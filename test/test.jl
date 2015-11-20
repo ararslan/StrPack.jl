a8aaaf08864bc28e5ed50a7fa43fd7c2cffda413
Alex A <ararslan@comcast.net>
2015-11-20 00:25:00 -0800

    Fixed some deprecation warnings in tests

--- test/test.jl
+++ test/test.jl
@@ -1,12 +1,13 @@
 using Base.Test
+using Compat
 using StrPack
 
 @struct type A
-    a::Uint8
+    a::UInt8
 end
 
 @struct type B
-    a::Array{Uint8,2}(2,2)
+    a::Array{UInt8,2}(2,2)
 end
 
 @struct type C
@@ -25,14 +26,14 @@ end
     a::Array{Float64,2}(3,2)
 end
 
-abstract abstractG
+abstract AbstractG
 
-@struct type G1 <: abstractG
-a::Uint8
+@struct type G1 <: AbstractG
+    a::UInt8
 end
 
-@struct immutable G2 <: abstractG
-a::Uint8
+@struct immutable G2 <: AbstractG
+    a::UInt8
 end
 
 @struct type Hvl_t
@@ -49,7 +50,7 @@ end
 
 @test roundtrip(A(0xbc))
 
-@test !roundtrip(B(zeros(Uint8, 0, 0)))
+@test !roundtrip(B(zeros(UInt8, 0, 0)))
 @test !roundtrip(B([0xab 0xcd]))
 @test roundtrip(B([0x01 0x02; 0x03 0x04]))
 @test !roundtrip(B([0x01 0x02 0xab; 0x03 0x04 0xcd; 0xac 0xbd 0xef]))
@@ -66,7 +67,7 @@ end
 
 @test roundtrip(F([1. 2.; 3. 4.; 5. 6.]))
 
-@test roundtrip(G1(uint8(0)))
-@test roundtrip(G2(uint8(0)))
+@test roundtrip(G1(UInt8(0)))
+@test roundtrip(G2(UInt8(0)))
 
-@test roundtrip(Hvl_t(uint64(0), ccall(:jl_environ, Ptr{Void}, (Int,), 0)))
+@test roundtrip(Hvl_t(UInt64(0), ccall(:jl_environ, Ptr{Void}, (Int,), 0)))
